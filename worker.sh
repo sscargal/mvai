5124de1a77cca2033a243b698979a23f66cd3a51
Steve Scargall <37674041+sscargal@users.noreply.github.com>
2025-04-12 14:30:13 -0600

    Update worker.sh

--- worker.sh
+++ worker.sh
@@ -13,23 +13,46 @@ sudo sh ./get-docker.sh
 
 systemctl enable docker && systemctl start docker
 
-echo "[K3S] Joining cluster"
-K3S_TOKEN=$(aws ssm get-parameter --name "/k3s/join-token" --with-decryption --query "Parameter.Value" --output text)
-if [ -z "$K3S_TOKEN" ]; then
-    echo "[ERROR] Failed to get the K3s Control Plane Token from AWS SSM. Exiting."
-    exit 1
-fi
+# Wait for the control plane to become ready
+echo "[WAIT] Waiting for Control Plane to become available"
 
-# Get the K3S URL from AWS SSM and fall back to using the IP
-K3S_URL=$(aws ssm get-parameter --name "/k3s/url" --with-decryption --query "Parameter.Value" --output text)
-if [ -z "$K3S_URL" ]; then
-    echo "[ERROR] Failed to get the K3s URL from AWS SSM." 
+timeout=600 # 10 minutes
+interval=30 # 30 seconds
+elapsed=0
+
+while [ "$elapsed" -lt "$timeout" ]; do
+    K3S_URL=$(aws ssm get-parameter --name "/k3s/url" --with-decryption --query "Parameter.Value" --output text)
+    if [ ! -z "$K3S_URL" ]; then
+        if curl -s -o /dev/null -w "%{http_code}" "$K3S_URL/healthz" | grep -q '200'; then
+            echo "[SUCCESS] Control Plane is available"
+            break
+        else
+            echo "[WAIT] Control Plane not yet ready, retrying..."
+        fi
+    else
+        echo "[WAIT] Control plane URL not yet available in SSM, retrying..."
+    fi
+    sleep "$interval"
+    elapsed=$((elapsed + interval))
+    echo "[WAIT] Elapsed Time: $((elapsed / 60))m $((elapsed % 60))s"
+done
+
+if [ "$elapsed" -ge "$timeout" ]; then
+    echo "[ERROR] Timeout: Control Plane not available after $((timeout / 60))m"
+    echo "[K3S] Trying to get the Control Plane IP Address"
     CONTROL_PLANE_IP=$(aws ec2 describe-instances --filters "Name=tag:Name,Values=MemVerge-ControlPlane" --query 'Reservations[*].Instances[*].PublicIpAddress' --output text)
     if [ -z "$CONTROL_PLANE_IP" ]; then
         echo "[ERROR] Failed to get the Control Plane IP Address from AWS SSM. Exiting."
         exit 1
     fi
     K3S_URL="https://${CONTROL_PLANE_IP}:6443"
+fi
+
+echo "[K3S] Joining cluster"
+K3S_TOKEN=$(aws ssm get-parameter --name "/k3s/join-token" --with-decryption --query "Parameter.Value" --output text)
+if [ -z "$K3S_TOKEN" ]; then
+   echo "[ERROR] Failed to get the K3s Control Plane Token from AWS SSM. Exiting."
+   exit 1
 fi
 
 # Install K3s as a worker node and join it to the cluster (Control Plane)
